ae0f4c75fe5a19c37da886b6ba3fa2eea76d348b
Níkolas Vargas <vargasnikolass@gmail.com>
2019-03-04 17:14:56 -0300

    renamed file, more tests with tdd and unit test

--- src/sale/test_sale.py
+++ src/sale/test_sale.py
@@ -1,21 +1,23 @@
+"""
+This module is for training.
+Contains unittest also have TDD implementations.
+"""
+
 from domain import User, Bid, Sale
 import unittest
 
-class TestClassifier(unittest.TestCase):
+class TestSale(unittest.TestCase):
     def setUp(self):
         self.testcase1 = User('john')
         self.testcase2 = User('angel')
         self.from_sale = Sale('Phone')
 
     def test_higher_and_lower_value_wagered_in_ascending_order(self):
-        john_bid = Bid(self.testcase1, 100)
-        angel_bid = Bid(self.testcase2, 200)
-
         LOWER_EXPECTED_VALUE = 100
         HIGHER_EXPECTED_VALUE = 200
 
-        self.from_sale.to_bet(john_bid)
-        self.from_sale.to_bet(angel_bid)
+        self.from_sale.to_bet(Bid(self.testcase1, 100))
+        self.from_sale.to_bet(Bid(self.testcase2, 200))
         self.assertEqual(LOWER_EXPECTED_VALUE, self.from_sale.lowest_bid)
         self.assertEqual(HIGHER_EXPECTED_VALUE, self.from_sale.higher_bid)
 
@@ -37,14 +39,26 @@ class TestClassifier(unittest.TestCase):
         self.assertEqual(HIGHER_EXPECTED_VALUE, self.from_sale.higher_bid)
 
     def test_return_same_value_when_sale_have_one_bid(self):
-        user_test_case = User('alone')
-        alone_bid = Bid(user_test_case, 5000)
-
         EXPECTED_VALUE = 5000
 
-        self.from_sale.to_bet(alone_bid)
+        self.from_sale.to_bet(Bid(User('alone'), 5000))
         self.assertEqual(EXPECTED_VALUE, self.from_sale.lowest_bid)
         self.assertEqual(EXPECTED_VALUE, self.from_sale.higher_bid)
+
+    # if is first bet, allow bettors bet
+    # TDD test
+    def test_first_bet_amount(self):
+        EXPECTED_VALUE = 1
+        self.from_sale.to_bet(Bid(self.testcase1, 100))
+        self.assertEqual(EXPECTED_VALUE, len(self.from_sale.bids))
+
+    # same bettor can't bet twice in sequence
+    # TDD test
+    def test_not_be_allow_multiple_bets_in_sequence(self):
+        self.from_sale.to_bet(Bid(self.testcase1, 100))
+        self.from_sale.to_bet(Bid(self.testcase1, 100))
+        user_name_obj_id = [id(bid.user.name) for bid in self.from_sale.bids]
+        self.assertNotEqual(user_name_obj_id[0], user_name_obj_id[1])
 
 if __name__ == "__main__":
     unittest.main()
