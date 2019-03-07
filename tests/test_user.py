1ca2ab00facb2d4e2063718e9f5c85e6b97b6a13
Níkolas Vargas <vargasnikolass@gmail.com>
2019-03-07 01:43:03 -0300

    tests for user class using pytest

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,20 @@
-from src.sale.domain import User
+"""
+This module is for training.
+Contains unittest also have TDD implementations.
+"""
+
+from src.sale.domain import User, Sale, Bid
+import pytest
+
+t_user = User('test_add_balance', 100)
+product = Sale('bell')
 
 
 def test_add_value_to_user_balance():
-    t_user = User('test_add_balance', 100)
     assert t_user.balance == 100
+
+
+def test_exception_user_cant_bet_more_than_balance():
+    with pytest.raises(ValueError):
+        bettor = Bid(t_user, 300)
+        bettor.user_can_bet()
